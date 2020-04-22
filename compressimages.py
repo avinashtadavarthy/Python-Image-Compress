1827bb5ec526ff8e8fbc44d0221514b177afec0d
avinashtadavarthy <avinash.tadavarthy@gmail.com>
2020-04-22 13:07:11 +0530

    Added time calculation

--- compressimages.py
+++ compressimages.py
@@ -1,9 +1,12 @@
 import os
 import cv2
+import timeit
+
+start = timeit.default_timer()
 
 os.mkdir("compressed")
 
-i=0
+i = 0
 for filename in os.listdir("./"):
     if os.path.isfile(filename):
         img = cv2.imread(filename)
@@ -21,10 +24,11 @@ for filename in os.listdir("./"):
             scale_percent = 50
         elif 4000 >= img.shape[1] >= 3000:
             scale_percent = 30
-        else: 
+        else:
             scale_percent = 20
 
-        print("scale percent:", scale_percent, img.shape[1] * scale_percent/100.0)
+        print("scale percent:", scale_percent,
+              img.shape[1] * scale_percent/100.0)
         width = int(img.shape[1] * scale_percent / 100)
         height = int(img.shape[0] * scale_percent / 100)
         dim = (width, height)
@@ -34,8 +38,13 @@ for filename in os.listdir("./"):
 
         # save image
         savedfilename = filename.split(".")[0]+"-min.jpg"
-        cv2.imwrite("compressed/"+savedfilename, resized, [int(cv2.IMWRITE_JPEG_QUALITY), 85])
+        cv2.imwrite("compressed/"+savedfilename, resized,
+                    [int(cv2.IMWRITE_JPEG_QUALITY), 85])
         print("file saved as:", savedfilename)
 
         print()
         i = i+1
+
+end = timeit.default_timer()
+
+print("Execution time: ", end - start)
